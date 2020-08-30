0f70bbbad5560df8c49d8feabc4f91a45eb73d29
unknown <astrid.lyre@outlook.com>
2020-08-30 08:25:23 -0400

    meta tags

--- src/services/yelpServices.js
+++ src/services/yelpServices.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const getBusinesses = async (term, location) => {
-  const request = axios.get(
-    `http://localhost:3001/api/businesses/${term}/${location}`
-  );
+  const request = axios.get(`/api/businesses/${term}/${location}`);
   return request.then((response) => response.data);
 };
 
