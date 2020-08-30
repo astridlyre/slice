90e4f6a0de414c75824d73e15313fa85c7f66979
unknown <astrid.lyre@outlook.com>
2020-08-30 07:45:01 -0400

    Inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./assets/css/App.css";
+import Header from "./components/Header";
+import SearchBarBig from "./components/SearchBarBig";
+import BusinessList from "./components/BusinessList";
+import BusinessModal from "./components/BusinessModal";
+import Footer from "./components/Footer";
+import { getBusinesses } from "./services/yelpServices";
 
-function App() {
+function App({ siteInfo }) {
+  const [filterBest, setFilterBest] = useState(true);
+  const [filterRated, setFilterRated] = useState(false);
+  const [filterReviewed, setFilterReviewed] = useState(false);
+  const [inputBusiness, setInputBusiness] = useState("");
+  const [inputWhere, setInputWhere] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [currentBusiness, setCurrentBusiness] = useState(null);
+
+  const handleInputBusiness = (event) => setInputBusiness(event.target.value);
+  const handleInputWhere = (event) => setInputWhere(event.target.value);
+  const handleFilterBest = () => {
+    setFilterBest(true);
+    setFilterRated(false);
+    setFilterReviewed(false);
+  };
+  const handleFilterRated = () => {
+    setFilterRated(true);
+    setFilterBest(false);
+    setFilterReviewed(false);
+  };
+  const handleFilterReviewed = () => {
+    setFilterReviewed(true);
+    setFilterBest(false);
+    setFilterRated(false);
+  };
+
+  const filteredByRating = [...searchResults].sort(
+    (a, b) => b.rating - a.rating
+  );
+
+  const filteredByReviewed = [...searchResults].sort(
+    (a, b) => b.review_count - a.review_count
+  );
+
+  const resultsToShow = () => {
+    if (filterBest) {
+      return searchResults;
+    } else if (filterRated) {
+      return filteredByRating;
+    } else if (filteredByReviewed) {
+      return filteredByReviewed;
+    } else {
+      return [];
+    }
+  };
+
+  const search = (event) => {
+    event.preventDefault();
+    getBusinesses(inputBusiness, inputWhere)
+      .then((response) => {
+        setSearchResults(response.businesses);
+        setCurrentBusiness(searchResults[0]);
+        setInputBusiness("");
+        setInputWhere("");
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full min-h-screen flex flex-col items-center bg-gray-100'>
+      <Header />
+      <section className='w-full bg-image flex justify-center items-start'>
+        <SearchBarBig
+          filterBest={filterBest}
+          filterReviewed={filterReviewed}
+          filterRated={filterRated}
+          handleFilterBest={handleFilterBest}
+          handleFilterRated={handleFilterRated}
+          handleFilterReviewed={handleFilterReviewed}
+          inputBusiness={inputBusiness}
+          inputWhere={inputWhere}
+          handleInputBusiness={handleInputBusiness}
+          handleInputWhere={handleInputWhere}
+          search={search}
+        />
+      </section>
+      {searchResults.length ? (
+        <BusinessList
+          searchResults={resultsToShow()}
+          setCurrentBusiness={setCurrentBusiness}
+        />
+      ) : (
+        <div className='w-full max-w-screen-md py-16 px-4 flex flex-col justify-center items-center'>
+          <h3 className='text-gray-400'>
+            To find the perfect place just search for any type of business and
+            let's go!
+          </h3>
+        </div>
+      )}
+      {currentBusiness && (
+        <BusinessModal
+          business={currentBusiness}
+          setCurrentBusiness={setCurrentBusiness}
+        />
+      )}
+      <Footer siteInfo={siteInfo} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const siteInfo = {
+  title: "slice",
+  social: {
+    instagram: "#",
+    facebook: "#",
+  },
+  contact: {
+    phone: "+1(812)-549-3029",
+    email: "support@slice.app",
+    address: "1324 Slice Ave",
+    city: "San Jose",
+    state: "CA",
+    zip: "29104",
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App siteInfo={siteInfo} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
